6d9ba0997d250e492cc756ed9171c601e9ba586d
Mehvish Ali <MoonWishLion@users.noreply.github.com>
2021-04-18 08:28:13 -0400

    Added Files via Upload

---
+++ Retro Spooky Jam - Folklore Phantoms/Assets/Scripts/Move2D.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{ 
+    public Animator anim;
+
+    public float moveSpeed = 5f;
+    public bool isGrounded = false;
+
+    // Start is called before the first frame update
+    // Use this for initialization
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+       
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+
+        /*
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.Play("Player_Run_Animation");
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.Play("Player_Run_Animation");
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            transform.position += movement * Time.deltaTime * moveSpeed;
+        }
+        */
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Retro Spooky Jam - Folklore Phantoms/Assets/Scripts/Obsolete/playerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float moveSpeed;
+    public float jumpForce;
+    private float moveInput;
+
+    private bool isGrounded = false;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    // Use this for initialization
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Jump();
+        // transform.position += movement * Time.deltaTime * moveSpeed;
+
+        /*
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.Play("Player_Run_Animation");
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.Play("Player_Run_Animation");
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            transform.position += movement * Time.deltaTime * moveSpeed;
+        }
+        */
+
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if(isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+}
